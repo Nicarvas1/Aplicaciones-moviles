c257f4a21be4570415dbee068f71dc26dc0770e5
Nicolas <nicolas.icv99@gmail.com>
2024-09-04 20:09:13 -0400

    login bilbao

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  icono = "oscuro"
+
+  constructor() {}
+
+  cambiarTema(){
+    if(this.icono == "oscuro"){
+      document.documentElement.style.setProperty("--fondo", "#373737")
+      this.icono = "claro"
+    }else{
+      document.documentElement.style.setProperty("--fondo", "#acda63")
+      this.icono = "oscuro"
+    }
+  }
+
+}
