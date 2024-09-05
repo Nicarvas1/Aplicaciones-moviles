4cdf395356e02fc4c29d9ddd9224c4853552b255
felirosalesb <feli.rosales@duocuc.cl>
2024-09-05 17:11:31 -0400

    Perfil actualizado

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
@@ -7,12 +8,20 @@ import { Router } from '@angular/router';
 })
 export class PerfilPage implements OnInit {
 
+  isConductorMode: boolean = false; // Declaración correcta de la variable
+
   constructor(private router: Router) { }
+
+  // Maneja el cambio en el ion-toggle
+  toggleChange(event: any) {
+    this.isConductorMode = event.detail.checked;
+  }
+
+  // Función para volver a la página anterior
   goBack() {
     this.router.navigate(['/']); // Navega a la ruta raíz, ajusta según tu estructura de rutas
   }
+
   ngOnInit() {
   }
-
-  
 }
