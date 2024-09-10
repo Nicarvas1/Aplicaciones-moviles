1b75284f975de58ed0bc0573f38c74cb308dbfa1
Nicolas <nicolas.icv99@gmail.com>
2024-09-10 13:55:56 -0300

    primera entrega lista

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ModalController, AnimationController, IonModal } from '@ionic/angular';
 
@@ -7,7 +7,7 @@ import { ModalController, AnimationController, IonModal } from '@ionic/angular';
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
 })
-export class PerfilPage implements OnInit {
+export class PerfilPage implements AfterViewInit {
   @ViewChild('modal', { static: false }) modal: IonModal | undefined;
   isConductorMode: boolean = false;
 
@@ -17,10 +17,15 @@ export class PerfilPage implements OnInit {
     private animationCtrl: AnimationController
   ) {}
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.setModalAnimations();
+  }
+
+  private setModalAnimations() {
+    if (!this.modal) return;
+
     const enterAnimation = (baseEl: HTMLElement) => {
       const root = baseEl.shadowRoot;
-
       if (!root) {
         throw new Error('Root shadow DOM not found');
       }
@@ -28,49 +33,51 @@ export class PerfilPage implements OnInit {
       const backdropElement = root.querySelector('ion-backdrop') as HTMLElement;
       const wrapperElement = root.querySelector('.modal-wrapper') as HTMLElement;
 
-      const backdropAnimation = this.animationCtrl
-        .create()
-        .addElement(backdropElement)
-        .fromTo('opacity', '0.01', 'var(--backdrop-opacity)');
+      if (backdropElement && wrapperElement) {
+        const backdropAnimation = this.animationCtrl
+          .create()
+          .addElement(backdropElement)
+          .fromTo('opacity', '0.01', 'var(--backdrop-opacity)');
 
-      const wrapperAnimation = this.animationCtrl
-        .create()
-        .addElement(wrapperElement)
-        .keyframes([
-          { offset: 0, opacity: '0', transform: 'scale(0)' },
-          { offset: 1, opacity: '0.99', transform: 'scale(1)' },
-        ]);
+        const wrapperAnimation = this.animationCtrl
+          .create()
+          .addElement(wrapperElement)
+          .keyframes([
+            { offset: 0, opacity: '0', transform: 'scale(0)' },
+            { offset: 1, opacity: '0.99', transform: 'scale(1)' },
+          ]);
 
-      return this.animationCtrl
-        .create()
-        .addElement(baseEl)
-        .easing('ease-out')
-        .duration(500)
-        .addAnimation([backdropAnimation, wrapperAnimation]);
+        return this.animationCtrl
+          .create()
+          .addElement(baseEl)
+          .easing('ease-out')
+          .duration(500)
+          .addAnimation([backdropAnimation, wrapperAnimation]);
+      } else {
+        return null; // Devolver null si alguno de los elementos no existe
+      }
     };
 
     const leaveAnimation = (baseEl: HTMLElement) => {
       const animation = enterAnimation(baseEl);
-      return animation.direction('reverse');
+      return animation ? animation.direction('reverse') : null;
     };
 
-    if (this.modal) {
-      this.modal.enterAnimation = enterAnimation as any;
-      this.modal.leaveAnimation = leaveAnimation as any;
-    }
+    this.modal.enterAnimation = enterAnimation as any;
+    this.modal.leaveAnimation = leaveAnimation as any;
   }
 
   toggleChange(event: any) {
     this.isConductorMode = event.detail.checked;
   }
 
-  goBack() {
-    this.router.navigate(['/']);
-  }
-
   async closeModal() {
     if (this.modal) {
       await this.modal.dismiss();
     }
+  }
+
+  goBack() {
+    this.router.navigate(['/']);
   }
 }
